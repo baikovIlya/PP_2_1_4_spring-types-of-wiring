5bad550e61d05863c1925504bfcc1ad378a1cc13
baikovIlya <baikusha23@gmail.com>
2025-02-17 19:43:08 +0300

    finish the chain

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,14 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    public Deth8() {}
+
+    @Override
+    public String toString() {
+        return "Вот Кощей и умер.";
+    }
+
 }
